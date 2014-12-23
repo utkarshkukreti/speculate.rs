c370596bdf170b10bb365ced68d3e10722daa366
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2014-12-23 19:49:46 +0530

    Register a basic compiler plugin named `speculate!`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
-#[test]
-fn it_works() {
+#![feature(plugin_registrar)]
+extern crate rustc;
+extern crate syntax;
+
+use rustc::plugin::Registry;
+use syntax::ast::TokenTree;
+use syntax::codemap::Span;
+use syntax::ext::base::{ExtCtxt, MacExpr, MacResult};
+use syntax::ext::build::AstBuilder;
+
+#[plugin_registrar]
+pub fn plugin_registrar(reg: &mut Registry) {
+    reg.register_macro("speculate", expand_speculate);
+}
+
+fn expand_speculate(cx: &mut ExtCtxt, sp: Span, _args: &[TokenTree]) -> Box<MacResult + 'static> {
+    MacExpr::new(cx.expr_uint(sp, 0))
 }
