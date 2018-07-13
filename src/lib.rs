3674bb7d55b4e330b9bac0050b6d7bd3e0596541
Grégoire Geis <git@gregoirege.is>
2018-07-13 15:32:01 +0200

    Now allowing non_snake_case in generated modules.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate rustc_plugin;
 extern crate syntax;
 
 use rustc_plugin::Registry;
+use syntax::attr::HasAttrs;
 use syntax::codemap::Span;
 use syntax::ext::base::{ExtCtxt, MacEager, MacResult};
 use syntax::parse::stream_to_parser;
@@ -25,8 +26,12 @@ fn expand_speculate(cx: &mut ExtCtxt, sp: Span, tokens: &[TokenTree]) -> Box<Mac
     let mod_name = format!("speculate #{}", sp.lo().0);
     
     let mut parser = stream_to_parser(cx.parse_sess(), tokens.iter().cloned().collect());
+
     let block = parser::parse(&mod_name, &mut parser);
-    let item = block.generate(cx, None);
+    let item = block.generate(cx, None).map_attrs(|mut attrs| {
+        attrs.push(quote_attr!(cx, #[allow(non_snake_case, unused_imports)])); 
+        attrs
+    });
 
     MacEager::items(SmallVector::one(item))
 }
